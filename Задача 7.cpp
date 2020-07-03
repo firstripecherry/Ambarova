3cd032ba9873d81bf076ba6a895f3fcf41085fab
firstripecherry <55585201+firstripecherry@users.noreply.github.com>
2020-07-03 16:12:38 +0300

    Add files via upload

---
+++ Задача 7.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class princess {
+public:
+	void meet_the_Prince() {
+		cout << "встретить принца" << endl;
+	}
+
+
+};
+class dog {
+public:
+	void bring_ball() {
+
+		cout << "принес мяч" << endl;
+	}
+};
+class princess_dog :public princess, public dog
+{
+};
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	princess_dog Vasiliy;
+	Vasiliy.bring_ball();
+	Vasiliy.meet_the_Prince();
+}
